816c8fe26110a65371e297cefae006744844efac
Wowzerf <79344383+Wowzerf@users.noreply.github.com>
2025-04-10 13:32:21 -0400

    Replace variable name to getChoice

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 function getComputerChoice() {
-    let x = Math.floor(Math.random() * 3) + 1;
+    let getChoice = Math.floor(Math.random() * 3) + 1;
 
-    if(x === 1) {
+    if(getChoice === 1) {
         return console.log("rock");
-    } else if (x === 2) {
+    } else if (getChoice === 2) {
         return console.log("paper");
     } else {
         return console.log("scissors")
