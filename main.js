1e4b38b04713d5e6f8d63be6ce9616abcd8502a1
Wowzerf <79344383+Wowzerf@users.noreply.github.com>
2025-04-10 12:52:13 -0400

    Make if and else statement inside function
    Whichever number is randomly chosen will correspond to a specific string and return it.

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 function getComputerChoice() {
     let x = Math.floor(Math.random() * 3) + 1;
+
+    if(x === 1) {
+        return console.log("rock");
+    } else if (x === 2) {
+        return console.log("paper");
+    } else {
+        return console.log("scissors")
+    }
+    
 }
